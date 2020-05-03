f0b52c91b59faf67ce4e4ea90d6273fdbcf980ef
Ilya Filimonau <ilfili@ttu.ee>
2020-05-03 19:18:06 +0300

    ADD Teacher model

---
+++ Duplet x school/Models/Teacher.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Duplet_x_school.Models
+{
+    public class Teacher
+    {
+        public int ID { get; set; }
+
+
+
+        [Required]
+
+        [Display(Name = "Last Name")]
+
+        [StringLength(50)]
+
+        public string LastName { get; set; }
+
+
+
+        [Required]
+
+        [Column("FirstName")]
+
+        [Display(Name = "First Name")]
+
+        [StringLength(50)]
+
+        public string FirstMidName { get; set; }
+
+
+
+        [DataType(DataType.Date)]
+
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+
+        [Display(Name = "Hire Date")]
+
+        public DateTime HireDate { get; set; }
+
+        [Display(Name = "Full Name")]
+
+        public string FullName
+
+        {
+
+            get { return LastName + ", " + FirstMidName; }
+
+        }
+
+        public bool IsClassroomTeacher { get; set; }
+
+        public ICollection<OptionalSubject> OptionalSubjects { get; set; }
+
+        public ClassAssignment ClassAssignment { get; set; }
+    }
+}
