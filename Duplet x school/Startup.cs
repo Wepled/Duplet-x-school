a241039ab2a967b1539ac3a549864609f272d09f
Ananjev Kirill <kirillananjev@gmail.com>
2020-05-14 13:53:48 +0300

    Editing mistakes

--- Duplet x school/Startup.cs
+++ Duplet x school/Startup.cs
@@ -41,6 +41,9 @@ namespace Duplet_x_school
 
             services.AddDbContext<SchoolContext>(options =>
                     options.UseSqlServer(Configuration.GetConnectionString("Duplet_x_schoolContext")));
+
+            services.AddDbContext<SchoolContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("Duplet_x_schoolContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
