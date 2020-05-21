2fe309a5da747a45fcb9b8c1395384072be3b4ab
Ananjev Kirill <kirillananjev@gmail.com>
2020-05-21 18:46:37 +0300

    Editing INDEX page in Teachers

---
+++ Duplet x school/Pages/Teachers/Index.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Duplet_x_school.Data;
+using Duplet_x_school.Models;
+
+namespace Duplet_x_school.Pages.Teachers
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Duplet_x_school.Data.SchoolContext _context;
+
+        public IndexModel(Duplet_x_school.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Teacher> Teacher { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Teacher = await _context.Teachers
+                .Include(c => c.TeacherSubjectAssignments).ThenInclude(c => c.Subject)
+                .Include(c => c.TeacherKabinetAssignment).ThenInclude(c => c.Kabinet)
+                .AsNoTracking()
+                .ToListAsync();
+        }
+    }
+}
