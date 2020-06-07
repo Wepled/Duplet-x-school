0178401e36040a08fff69076b543a1366d0277b9
Ilya Filimonau <ilfili@ttu.ee>
2020-06-07 18:16:18 +0300

    Add SchoolClass page

---
+++ Duplet x school/Pages/SchoolClasses/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Duplet_x_school.Data;
+using Duplet_x_school.Models;
+
+namespace Duplet_x_school.Pages.SchoolClasses
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly Duplet_x_school.Data.SchoolContext _context;
+
+        public DetailsModel(Duplet_x_school.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public SchoolClass SchoolClass { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            SchoolClass = await _context.SchoolClasses.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (SchoolClass == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
